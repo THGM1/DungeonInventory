0a73eef09f41f216e58ec55c33d6136342b271b1
THGM1 <jennypsh@naver.com>
2025-03-25 10:28:01 +0900

    [Feat] 장비 스탯 구분

--- Assets/Scripts/UI/UIStatus.cs
+++ Assets/Scripts/UI/UIStatus.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -25,13 +26,36 @@ public class UIStatus : MonoBehaviour
 
     public void UpdateUI()
     {
-        atk.text = player.Atk.ToString();
-        def.text = player.Def.ToString();
+        atk.text = $"{player.Atk.ToString()}{EquipStat(0)}";
+        def.text = $"{player.Def.ToString()}{EquipStat(1)}";
         health.text = $"{player.CurHealth}/{player.MaxHealth}";
-        critical.text = player.Critical.ToString();
+        critical.text = $"{player.Critical.ToString()}{EquipStat(2)}";
     }
     private void AddButtonListener()
     {
         closeBtn.onClick.AddListener(UIManager.Instance.OpenStatus);
+    }
+
+    public string EquipStat(int num)
+    {
+        int stat = 0;
+        switch (num)
+        {
+            case 0:
+                stat = GameManager.Instance.equipAtk;
+                break;
+            case 1:
+                stat = GameManager.Instance.equipDef;
+                break;
+            case 2:
+                stat = GameManager.Instance.equipCri;
+                break;
+            default:
+                stat = 0;
+                break;
+        }
+        string str = $" (+{stat.ToString()})";
+        if (stat != 0) return str;
+        return null;
     }
 }
